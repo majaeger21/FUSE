684f26ef6d19e2d4407f6c02c766c4e2d372cc46
Meyli Jaeger <meylij@gmail.com>
2025-06-04 17:31:51 -0700

    .h mistake

--- mirror_fs.h
+++ mirror_fs.h
@@ -16,7 +16,7 @@ void mir_usage();
 void mir_path(char fpath[PATH_MAX], const char *path);
 
 // Encryption helper functions
-void derive_key(const char *passphras);
+void derive_key(const char *passphrase);
 int encrypt_data(const unsigned char *plaintext, int plaintext_len, 
                  unsigned char *ciphertext);
 int decrypt_data(const unsigned char *ciphertext, int ciphertext_len,
